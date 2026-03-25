c218f5bbf9d790d1a0753350c4acac5683302c87
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-25 22:02:48 +0500

    Task2

---
+++ Tyuiu.KopylskikhVM.Sprint1.Task2.V21/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.KopylskikhVM.Sprint1.Task2.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Копыльских В. М. | НТМ-24-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнила: Копыльских Виктория Максимовна | НТм-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int value, valueTwo;
+        Console.WriteLine("Введите значение первой стороны треугольника:");
+        value = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите значение второй стороны треугольника:");
+        valueTwo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.CalculateRectangleSquare(value, valueTwo));
+
+        Console.ReadLine();
+    }
+}
