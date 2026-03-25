9a199227c18af2ee4035b3248dffec6f5b5ba393
Виктория Копыльских <vika_2001_vika@mail.ru>
2026-03-25 10:51:34 +0500

    Task0

---
+++ Tyuiu.KopylskikhVM.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using Tyuiu.KopylskikhVM.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.KopylskikhVM.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(5, res);
+        }
+       
+    }
+}
